86d6b90d8a9b69f846f0c3fe66f9b70d46ffd64a
jinchangz <jinchangz1@gmail.com>
2024-08-18 08:46:27 +0900

    second commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,18 @@
 import { defineConfig } from 'astro/config';
+import react from "@astrojs/react";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react()],
+  output: "hybrid",
+  adapter: cloudflare({
+    imageService: 'cloudflare',
+    platformProxy: {
+      enabled: true,
+      configPath: 'wrangler.toml',
+      experimentalJsonConfig: true,
+      persist: './.cache/wrangler/v3',
+    },
+  })
+});
